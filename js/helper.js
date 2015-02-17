805514ebe65a3913e11486704615d02df562eed4
unknown <evil@curs.es>
2015-02-16 17:17:49 -0800

    uncommented vanilla js map initialization

--- js/helper.js
+++ js/helper.js
@@ -239,7 +239,7 @@ window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
